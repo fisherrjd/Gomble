4171a0b1430b7f81cc8acdb48271e231aa4d13e3
fisherrjd <fisherrjd@gmail.com>
2022-07-01 13:13:21 -0600

    added option for more dice in the throws

--- dice.py
+++ dice.py
@@ -6,20 +6,17 @@ class Die:
     def __init__(self, sides):
         self.sides = sides
 
-        
+class Throw:
+    def __init__(self, totDice):
+        totDice = totDice
+        total = 0;
+
     def throw(self):
-        return random.randint(1,6)
+        for x in totDice:
+             total += random.randint(1,6)
 
 
 crapDie = Die(6)
 
-tot = crapDie.throw
-print(tot)
-    
-
-
-
-
-
-        
-
+tot =  crapDie.throw()
+print (tot)
