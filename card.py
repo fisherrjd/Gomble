d2ff25764816d86aa2b688deec86fb13eea9302f
fisherrjd <fisherrjd@gmail.com>
2022-07-02 09:00:38 -0600

    working on showing top card

--- card.py
+++ card.py
@@ -1,9 +1,3 @@
-from os import readlink
-import random
-from typing import List, Union, Tuple
-
-
-
 class Card:
     def __init__(self, suit, rank):
         self.ranks = [None, "Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
@@ -38,12 +32,11 @@ class Deck:
     
     def build(self):
         for x in ["d", "c", "h", "s"]:
-            for v in range(1,14):
+            for v in range(1,10):
                 self.cards.append(Card(x, v))    
     def shuffle(self):
         print("shuffle")
 
-    def show(self):
-        for c in self.cards:
-            c.show
-
+    def showTop(self):
+        topCard = Card(self.cards[1].getSuit, self.cards[1].getRank)
+        print(topCard.show)
